d91e86d246f3d3c6c992639210b5814400fb0713
Robert Sparks <robert.william.sparks@gmail.com>
2021-03-14 19:08:15 +0000

    added week3 coloring and a simple greedy solution

---
+++ week3_coloring/solver.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from collections import defaultdict
+
+def solve_it(input_data):
+    # Modify this code to run your optimization algorithm
+
+    # parse the input
+    lines = input_data.split('\n')
+
+    first_line = lines[0].split()
+    node_count = int(first_line[0])
+    edge_count = int(first_line[1])
+
+    edges = defaultdict(list)
+    for i in range(1, edge_count + 1):
+        line = lines[i]
+        u, v = map(int, line.split())
+        edges[u].append(v)
+        edges[v].append(u)
+
+    colors = [None for _ in range(node_count)]
+    # straight forward greedy algorithm
+    # first node has color 0
+    upperbound = set(range(node_count))
+    colors[0] = 0
+    for i in range(1, node_count):
+        neighbours = {colors[j] for j in edges[i] if colors[j] != None}
+        lowest_available = min(upperbound - neighbours)
+        colors[i] = lowest_available
+
+
+    # prepare the solution in the specified output format
+    colors_count = len(set(colors))
+    output_data = str(colors_count) + ' ' + str(0) + '\n'
+    output_data += ' '.join(map(str, colors))
+
+    return output_data
+
+
+import sys
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        file_location = sys.argv[1].strip()
+        with open(file_location, 'r') as input_data_file:
+            input_data = input_data_file.read()
+        print(solve_it(input_data))
+    else:
+        print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)')
+
